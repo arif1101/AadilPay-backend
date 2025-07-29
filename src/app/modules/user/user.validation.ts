5e231698d0862c6d4d491c580f17a80c4eba51cb
arif1101 <arifurrahmanarif223@gmail.com>
2025-07-29 20:51:18 +0600

    get my wallet

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -11,7 +11,8 @@ export const createUserZodSchema = z.object({
     .string({ error: "Email must be a string." })
     .email({ message: "Invalid email address format." })
     .min(5, { message: "Email must be at least 5 characters long." })
-    .max(100, { message: "Email cannot exceed 100 characters." }),
+    .max(100, { message: "Email cannot exceed 100 characters." })
+    .optional(),
 
   password: z
     .string({ error: "Password must be a string." })
