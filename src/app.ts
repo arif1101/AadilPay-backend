d6d17eb82b0c711f65601b7411d71b327c432414
arif1101 <arifurrahmanarif223@gmail.com>
2025-08-23 06:45:30 +0600

    login, role base dashboard and routing setup

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,17 @@ import express, { Request, Response } from "express";
 import notFound from "./app/middlewares/notFound";
 import { router } from "./app/routes";
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
+import cookieParser from 'cookie-parser';
 
 const app = express()
 
+app.use(cookieParser())
 app.use(express.json())
 app.set("trust proxy",1)
-app.use(cors())
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use("/api", router)
 app.get("/", (req: Request, res: Response) => {
     res.status(200).json({
--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -11,11 +11,36 @@ interface EnvConfig {
     BCRYPT_SALT_ROUND: string,
     ADMIN_EMAIL: string,
     ADMIN_PASSWORD: string,
-    PHONE : string
+    PHONE : string,
+    SSL: {
+        STORE_ID: string,
+        STORE_PASS: string,
+        SSL_PAYMENT_API: string,
+        SSL_VALIDATION_API: string,
+        SSL_SUCCESS_FRONTEND_URL: string,
+        SSL_FAIL_FRONTEND_URL: string,
+        SSL_CANCEL_FRONTEND_URL: string,
+        SSL_SUCCESS_BACKEND_URL: string,
+        SSL_FAIL_BACKEND_URL: string,
+        SSL_CANCEL_BACKEND_URL: string,
+        SSL_IPN_URL: string
+    },
 }
 
 const loadEnvVariables = (): EnvConfig => {
-    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV","BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "ADMIN_EMAIL", "ADMIN_PASSWORD", "PHONE"];
+    const requiredEnvVariables: string[] = ["PORT", "DB_URL", "NODE_ENV","BCRYPT_SALT_ROUND", "JWT_ACCESS_EXPIRES", "JWT_ACCESS_SECRET", "ADMIN_EMAIL", "ADMIN_PASSWORD", "PHONE",
+        "SSL_STORE_ID",
+        "SSL_STORE_PASS",
+        "SSL_PAYMENT_API",
+        "SSL_VALIDATION_API",
+        "SSL_SUCCESS_FRONTEND_URL",
+        "SSL_FAIL_FRONTEND_URL",
+        "SSL_CANCEL_FRONTEND_URL",
+        "SSL_SUCCESS_BACKEND_URL",
+        "SSL_FAIL_BACKEND_URL",
+        "SSL_CANCEL_BACKEND_URL",
+        
+    ];
 
     requiredEnvVariables.forEach(key => {
         if (!process.env[key]) {
@@ -33,7 +58,20 @@ const loadEnvVariables = (): EnvConfig => {
         JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
         ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
         ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
-        PHONE: process.env.PHONE as string
+        PHONE: process.env.PHONE as string,
+        SSL: {
+            STORE_ID: process.env.SSL_STORE_ID as string,
+            STORE_PASS: process.env.SSL_STORE_PASS as string,
+            SSL_PAYMENT_API: process.env.SSL_PAYMENT_API as string,
+            SSL_VALIDATION_API: process.env.SSL_VALIDATION_API as string,
+            SSL_SUCCESS_FRONTEND_URL: process.env.SSL_SUCCESS_FRONTEND_URL as string,
+            SSL_FAIL_FRONTEND_URL: process.env.SSL_FAIL_FRONTEND_URL as string,
+            SSL_CANCEL_FRONTEND_URL: process.env.SSL_CANCEL_FRONTEND_URL as string,
+            SSL_SUCCESS_BACKEND_URL: process.env.SSL_SUCCESS_BACKEND_URL as string,
+            SSL_FAIL_BACKEND_URL: process.env.SSL_FAIL_BACKEND_URL as string,
+            SSL_CANCEL_BACKEND_URL: process.env.SSL_CANCEL_BACKEND_URL as string,
+            SSL_IPN_URL: process.env.SSL_IPN_URL as string
+        },
     }
 }
 
